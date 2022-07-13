8776558f07e30c1f41b163e55d7aef92d1351efe
aliajjoub2 <83879405+aliajjoub2@users.noreply.github.com>
2022-07-13 15:27:48 +0200

    add function changeIndex and add List

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Wisdoms Project',
       theme: ThemeData(
-      
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
@@ -23,62 +22,123 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
- 
- const MyHomePage({Key? key}) : super(key: key);
-
-
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
+  List verse = [
+    {
+      "line1": " الرقم واحد لكي نطول الكتابة كتابة كتابة كتابة كتابة  لا تشكُ للناسِ جرحاً أنتَ صاحبه",
+      "line2": "لا يؤلمُ الجرحَ إلا منْ به ألم"
+    },
+    {
+      "line1": "1- شَكْوَاكَ لِلنَّاسِ يا ابنَ النَّاس منْقصَةٌ",
+      "line2": "ومَن مِنَ النَّاسِ صَاحِ مَا بِهِ سَقَمُ"
+    },
+    {
+      "line1": "2- فَإِنْ شَكَوْتَ لِمَنْ طَابَ الزَّمَانُ لَهُ",
+      "line2": "عَيْنَاكَ تَغْلِي وَمَنْ تَشْكُو لَهُ صَنَمُ"
+    },
+    {
+      "line1": "3- وَإِذَا شَكَوْتَ لِمَنْ شَكْوَاكَ تُسْعِدُهُ",
+      "line2": "أَضَفْتَ جُرْحًا لِجُرْحِكَ اِسْمُهُ النَّدَمُ"
+    },
+    {
+      "line1": "هل المواساه يوماً حرت وطناً",
+      "line2": "أم التعازى بديل ان هو العلمُ"
+    },
+    {
+      "line1": "من يندب الحظ يطفئ عين همته",
+      "line2": "لاعين للحظ أن لم تبصر الهمم"
+    },
+    {
+      "line1": "وَمِنْ سِوَى اللهِ نَأْوِي تَحْتَ سِدْرَتِهِ",
+      "line2": "وَنَسْتَغِيثُ بِهِ عَوِّنَا وَنَعْتَصِمُ"
+    },
+    {
+      "line1": "كُن فَيْلَسُوفًَا ترى أنَّ الجميعَ هُنَا",
+      "line2": "يتقاتلون على عَدَمٍ وهُم عَدَمُ!!"
+    },
+  ];
+
+  int myIndex = 0;
+
+  changeIndex(String myDirection) {
+    if (myDirection == 'right') {
+      setState(() {
+        if (myIndex == (verse.length - 1)) {
+          myIndex = 0;
+        } else {
+          myIndex++;
+        }
+      });
+    } else if (myDirection == 'left') {
+      setState(() {
+        if (myIndex == 0) {
+          myIndex = (verse.length - 1);
+        } else {
+          myIndex--;
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(
-        
-        title: const Text('Wisdoms'),
-        centerTitle: true,
-      ),
-      body: Center(
-        
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('the first widsom'),
-            const SizedBox(height: 20,),
-            Text('the first widsom'),
-            const SizedBox(height: 60,),
-            Row(
+        appBar: AppBar(
+          title: const Text('Wisdoms'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: SizedBox(
+            width: 900,
+            child: Column(
+              
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                FloatingActionButton(onPressed: (){
-
-                },
-              
-                backgroundColor: Colors.amber,
-                  child: const Icon(Icons.arrow_left, color: Colors.black,),
-                
+                Text(verse[myIndex]['line1']),
+                const SizedBox(
+                  height: 20,
                 ),
-                const SizedBox(width: 20,),
-                FloatingActionButton(onPressed: (){
-                  
-                },
-                
-                backgroundColor: Colors.amber,
-                child: const Icon(Icons.arrow_right, color: Colors.black,),
-                
+                Text(verse[myIndex]['line2']),
+                const SizedBox(
+                  height: 60,
                 ),
-
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FloatingActionButton(
+                      onPressed: () {
+                        changeIndex('left');
+                      },
+                      backgroundColor: Colors.amber,
+                      child: const Icon(
+                        Icons.arrow_left,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    FloatingActionButton(
+                      onPressed: () {
+                        changeIndex('right');
+                      },
+                      backgroundColor: Colors.amber,
+                      child: const Icon(
+                        Icons.arrow_right,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-
-          ],
-        ),
-    ));
+          ),
+        ));
   }
 }
